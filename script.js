101a23e518ab944c9bb59476d10ee95f6b1e9c5c
Alhenagenesa <165838619+Alhenagenesa@users.noreply.github.com>
2024-04-15 17:14:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function openNav() {
+  document.body.style.overflow = "hidden";
   document.getElementById("sidenav").style.width = "100%";
   document.getElementById("overlay").classList.add('active');
 }
 
 function closeNav() {
+  document.body.style.overflow = "";
   document.getElementById("sidenav").style.width = "0";
   document.getElementById("overlay").classList.remove('active');
 }
